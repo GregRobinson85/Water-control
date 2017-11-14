395bd6a0f01bab90c572a1626826de51d085750e
GregRobinson85 <gregr@carpentrybythebook.com>
2017-11-13 22:02:55 -0800

    success with mega
    first success with thermocouple, mega and  lcd all in one, have yet to include any motor function or even mock motor function

--- buttons_meet_proto_with_lcd.ino
+++ buttons_meet_proto_with_lcd.ino
@@ -13,8 +13,7 @@
 #define ONE_WIRE_BUS 2 //2 is digital pin number
 #define WAKE 13
 
-#include <OneWire.h>
-#include <DallasTemperature.h>
+
 #include <LiquidCrystal.h>
 #include<ButtonV2.h>
 
@@ -39,7 +38,6 @@ int switchState = 0;
 int prevSwitchState = 0;
 
 int target =200;
-int Temp =0;
 
 int absPosCold = 50;
 int absPosHot = 50;
@@ -51,8 +49,13 @@ int Hot = 0;
 
 LiquidCrystal lcd(12, A5, A1, A2, A3, A4);//(RS,E,DB4,DB5,DB6,DB7) don't forget the pins are backward from silkscreen!
 
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire); 
+#include "max6675.h"
+
+int ktcSO = 22;
+int ktcCS = 24;
+int ktcCLK = 26;
+
+MAX6675 ktc(ktcCLK, ktcCS, ktcSO);
 
 void setup(){
    // set up the number of columns and rows on the LCD
@@ -88,25 +91,36 @@ void setup(){
   Button4.SetStateAndTime(LOW);
   Button5.SetStateAndTime(LOW);
   
+ 
   Serial.println("Button_for_press_cases");
   Serial.println("ready");
 
 //turn on power save mode
 digitalWrite(WAKE, 0);
 
-sensors.begin();
 
-Serial.println("proto_with_lcd");
+Serial.println("buttons_meet_proto_with_lcd");
+
+
+  // give the MAX a little time to settle
+  delay(500);
 }
-
-
 void loop(){
   
-  
-  
+//ktype thermocouple code made more complicated with the telemetry injunction
 
+  
+  	float temp = ktc.readFahrenheit();
+
+/*	char temp_text[30];
+
+	dtostrf(temp, 10, 10, temp_text);
+
+	char text[31];
+	snprintf(text, 31, "%s", temp_text);
+	Serial.println(text);
+*/
  
- //this is the beginning of a trial block insertion; Temp=target should be moved to the end of this block
  
   
   
@@ -117,15 +131,15 @@ void loop(){
     case WAITING:
       break;
     case PRESSED:
-      Serial.println("pressed 1 time");
+      Serial.println("tempup pressed 1 time");
       target=target+1;
       break;
     case DOUBLE_PRESSED:
-      Serial.println("pressed 2 times");
+      Serial.println("tempup pressed 2 times");
       target=target+2;
       break;
     case MULTI_PRESSED:
-      Serial.println("pressed 3 times");
+      Serial.println("tempup pressed 3 times");
       target=target+3;
       break;
     case HELD:
@@ -141,15 +155,15 @@ switch (type2)
     case WAITING:
       break;
     case PRESSED:
-      Serial.println("pressed 1 time");
+      Serial.println("tempdown pressed 1 time");
       target=target-1;
       break;
     case DOUBLE_PRESSED:
-      Serial.println("pressed 2 times");
+      Serial.println("tempdown pressed 2 times");
       target=target-2;
       break;
     case MULTI_PRESSED:
-      Serial.println("pressed 3 times");
+      Serial.println("tempdown pressed 3 times");
       target=target-3;
       break;
     case HELD:
@@ -164,17 +178,17 @@ switch (type2)
     case WAITING:
       break;
     case PRESSED:
-      Serial.println("pressed 1 time");
+      Serial.println("M1 pressed 1 time");
       target = Mem1;
       break;
     case DOUBLE_PRESSED:
-      Serial.println("pressed 2 times, so what?");
+      Serial.println("M1 pressed 2 times, so what?");
       break;
     case MULTI_PRESSED:
-      Serial.println("pressed 3 times, so what?");
+      Serial.println("M1 pressed 3 times, so what?");
       break;
     case HELD:
-      Mem1 = Temp;
+      Mem1 = temp;
       Serial.print("Mem 1 reset to: ");
       Serial.println(Mem1);
       break;
@@ -187,17 +201,17 @@ switch (type2)
     case WAITING:
       break;
     case PRESSED:
-      Serial.println("pressed 1 time");
+      Serial.println("M2 pressed 1 time");
       target = Mem2;
       break;
     case DOUBLE_PRESSED:
-      Serial.println("pressed 2 times, so what?");
+      Serial.println("M2 pressed 2 times, so what?");
       break;
     case MULTI_PRESSED:
-      Serial.println("pressed 3 times, so what?");
+      Serial.println("M2 pressed 3 times, so what?");
       break;
     case HELD:
-      Mem2 = Temp;
+      Mem2 = temp;
       Serial.print("Mem 2 reset to: ");
       Serial.println(Mem2);
       break;
@@ -211,32 +225,26 @@ switch (type2)
     case WAITING:
       break;
     case PRESSED:
-      Serial.println("pressed 1 time");
+      Serial.println("M3 pressed 1 time");
       target = Mem3;
       break;
     case DOUBLE_PRESSED:
-      Serial.println("pressed 2 times, so what?");
+      Serial.println("M3 pressed 2 times, so what?");
       break;
     case MULTI_PRESSED:
-      Serial.println("pressed 3 times, so what?");
+      Serial.println("M3 pressed 3 times, so what?");
       break;
     case HELD:
-      Mem3 = Temp;
+      Mem3 = temp;
       Serial.print("Mem 3 reset to: ");
       Serial.println(Mem3);
       break;
      
   }
   
-   Serial.println(target);
    
-   //this is the end of the button block//
-   //this block slows everything down to 1hz
-    sensors.requestTemperatures();
- Temp = (((sensors.getTempCByIndex(0))*1.8)+32); 
 
- 
- 
+
  while(absPosCold <20){
    for(absPosCold=19; absPosCold<35; absPosCold++){
     digitalWrite(4, HIGH);
@@ -263,22 +271,22 @@ switch (type2)
   
  
    Serial.print("Current Temp:  ");
-   Serial.print(Temp);
+   Serial.print(temp);
    Serial.print(",  Target:  ");
    Serial.print(target);
    Serial.print(",  Differential:  ");
-   Serial.println(target-Temp);
+   Serial.println(target-temp);
    
    
    int switchState=analogRead(onSwitch);
-   Serial.println(switchState); 
+  // Serial.println(switchState); 
    
    if(switchState > 1000){
      
      digitalWrite(WAKE, 1);
      
    //keep 1 degree margin
-   if(Temp > (target+1)){
+   if(temp > (target+1)){
     digitalWrite(6, LOW);
     digitalWrite(5, HIGH);
     delay(1);          
@@ -292,7 +300,7 @@ switch (type2)
     }
     
     
-    if(Temp < (target-1)){ 
+    if(temp < (target-1)){ 
     digitalWrite(4, LOW);
     digitalWrite(3, HIGH);
     delay(1);          
@@ -312,8 +320,10 @@ switch (type2)
 lcd.clear();
 lcd.setCursor(0, 0);
 lcd.print("Temp:  ");
-lcd.print(Temp);
+lcd.print(temp);
 lcd.setCursor(0, 1);
 lcd.print("Target: ");
 lcd.print(target);
+
+delay(180);
 }
